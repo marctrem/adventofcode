3bd82ac8deada6c52c4fda5d3a488e92dbf926b4
Marc-André Tremblay <marcandre.tr@gmail.com>
2015-12-09 16:24:27 -0500

    Day 9! TSP!!!

---
+++ src/day09/core.clj
@@ -0,0 +1,32 @@
+(ns day09.core
+  (:require [clojure.math.combinatorics :as combo]
+            [clojure.string :as str]))
+
+
+(defn parse [data]
+
+  (reduce (fn [akku item]
+            (let [tokens (str/split item #" ")
+                  src (nth tokens 0)
+                  dest (nth tokens 2)
+                  distance (Integer. (nth tokens 4))]
+
+              (update-in akku [src] (fn [old] (if (nil? old)
+                                                {dest distance}
+                                                (assoc old dest distance))))))
+          {} (str/split-lines data)))
+
+(defn generic [goal-fn input]
+  "Solves part one of day nine."
+  (let [data (slurp input)
+        routes (parse data)
+        cities (set (concat (keys routes) (flatten (map keys (vals routes)))))]
+
+    (reduce goal-fn (map (partial reduce +) (map (fn [path]
+                                                   (let [jumps (partition 2 1 path)]
+                                                     (map (fn [[src dest]] (or (get-in routes [src dest])
+                                                                               (get-in routes [dest src]))) jumps)))
+                                                 (combo/permutations cities))))))
+
+(def part1 (partial generic min))
+(def part2 (partial generic max))
